ee7d62a43c21bbc27b418c6576284c6c98119f70
ianyulistio <ianyulistio.swordartonline@gmail.com>
2018-05-24 00:08:58 +0700

    Haiii Mudah mudahan bisa

---
+++ resources/views/inc/intro.blade.php
@@ -0,0 +1,13 @@
+<section id="intro" class="intro">
+
+    <div class="slogan">
+      <h2>WELCOME TO <span class="text_color">OUR CLASSIFICATION WEBSITE</span> </h2>
+      <h4>WE ARE</h4>
+    </div>
+    <div class="page-scroll">
+      <a href="#service" class="btn btn-circle">
+				<i class="fa fa-angle-double-down animated"></i>
+			</a>
+    </div>
+  </section>
+  
